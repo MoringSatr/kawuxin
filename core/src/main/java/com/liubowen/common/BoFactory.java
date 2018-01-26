c97c2c7cb098afdf456e0f35a94d3d182035ad22
liubowen <liubowen>
2018-01-26 20:47:39 +0800

    aa

---
+++ core/src/main/java/com/liubowen/common/BoFactory.java
@@ -0,0 +1,10 @@
+package com.liubowen.common;
+
+/**
+ * @author liubowen
+ * @date 2018/1/19 21:16
+ * @description
+ */
+public class BoFactory {
+
+}
---
+++ core/src/main/java/com/liubowen/domain/user/User.java
@@ -0,0 +1,9 @@
+package com.liubowen.domain.user;
+
+/**
+ * @author liubowen
+ * @date 2018/1/19 21:13
+ * @description
+ */
+public class User {
+}
